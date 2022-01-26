a4f308444c6d60e00450df4e1aa226e03011c712
TakKokubu <kokubu.tk@gmail.com>
2022-01-27 08:42:11 +0900

    users apiへデータ送信

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
   def index
-    # イベントデータの一覧を返す
     render json: User.all
   end
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def create
+    user = User.new
+    if user.save
+      render json: user
+    else
+      render json: user.errors, status: 422
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
 end
