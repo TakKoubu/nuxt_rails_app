930a7cf73bf8b141db9d3d89e393c92814eea06b
TakKokubu <kokubu.tk@gmail.com>
2022-01-27 09:42:20 +0900

    削除用apiの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
   post '/users', to: 'users#create'
+  put '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+  resources :users, only: [:index, :show, :create, :update, :destroy]
 end
