97031a55cd68b600b1cd40d99ffb7690ec4a286e
TakKokubu <kokubu.tk@gmail.com>
2022-01-28 17:03:16 +0900

    memo controllerの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace 'api' do
     resources :users, only: [:index, :show, :create, :update, :destroy]
+    resources :memos, only: [:index, :create, :destroy]
   end
 end
