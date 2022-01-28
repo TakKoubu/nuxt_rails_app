bdaa0d3885dbc008c50c1d313ce8fe80071bd4fd
TakKokubu <kokubu.tk@gmail.com>
2022-01-28 20:59:13 +0900

    jwtログインの修正

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,10 +10,13 @@ module Api
 
     def create
       user = User.new(user_params)
-      if user.save
-        render json: user
+      if user.valid?
+        payload = {user_id: user.id}
+        token = encode_token(payload)
+        render json: {user: user.user_id, jwt: token}
+        binding.pry
       else
-        render json: user.errors, status: 422
+        render json: {errors: user.errors.full_messages},status: :not_acceptable
       end
     end
 
---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  class AuthController < ApplicationController
+    skip_before_action :require_login, only: [:login, :auto_login]
+    def login
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        payload = {user_id: user.id,email: user.email}
+        token = encode_token(payload)
+        render json: {jwt: token,success: "Welcome back, #{user.email}"}
+      else
+        response_unauthorized
+      end
+    end
+
+    def auto_login
+      if session_user 
+        render json: session_user
+      else
+        render json: {errors: "No user Logged In"}
+      end
+    end
+  end
+end
