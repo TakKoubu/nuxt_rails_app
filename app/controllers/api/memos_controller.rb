43ad36c8a31efe82abc4a2854e6b16d5f6697931
TakKokubu <kokubu.tk@gmail.com>
2022-02-03 13:32:37 +0900

    memo機能の作成

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -7,8 +7,6 @@ module Api
     def create
       memo = Memo.new(memo_params)
       memo.user_id = 1
-      memo.content.to_s
-      binding.pry
       if memo.save
         render json: memo
       else
